78cc98a899b7cd7d5efd79efdeeedcbdbd8fc07a
PolyFacto <164651706+PolyFacto@users.noreply.github.com>
2026-05-26 18:38:14 +0200

    Optimization and prototype map cleanup

--- addons/tiny_draw_debug/tiny_draw_debug.gd
+++ addons/tiny_draw_debug/tiny_draw_debug.gd
@@ -1,9 +1,13 @@
 @tool
 extends Node
 
-const EPSILON = 1e-6
+const EPSILON: float = 1e-6
 
 var _lines: Array = []
+
+var _immediate_points: PackedVector3Array = PackedVector3Array()
+var _immediate_colors: PackedColorArray = PackedColorArray()
+
 var _mesh: ImmediateMesh
 var _instance: RID
 var _material: StandardMaterial3D
@@ -30,60 +34,71 @@ func _exit_tree() -> void:
 	if _instance.is_valid():
 		RenderingServer.free_rid(_instance)
 
-
 func _process(delta: float) -> void:
-	var world := get_viewport().find_world_3d()
+	var world: World3D = get_viewport().find_world_3d()
 	if world:
 		RenderingServer.instance_set_scenario(_instance, world.scenario)
 	
 	_mesh.clear_surfaces()
 	
-	if _lines.is_empty():
-		return
-		
-	var i := _lines.size() - 1
+	var i: int = _lines.size() - 1
 	while i >= 0:
 		_lines[i].time -= delta
 		if _lines[i].time <= 0.0:
 			_lines.remove_at(i)
 		i -= 1
-		
-	if _lines.is_empty():
+	
+	if _lines.is_empty() and _immediate_points.is_empty():
 		return
 		
 	_mesh.surface_begin(Mesh.PRIMITIVE_LINES, _material)
-	for line in _lines:
+	
+	if not _immediate_points.is_empty():
+		var arrays: Array = []
+		arrays.resize(Mesh.ARRAY_MAX)
+		arrays[Mesh.ARRAY_VERTEX] = _immediate_points
+		arrays[Mesh.ARRAY_COLOR] = _immediate_colors
+		
+		for pt_idx: int in range(_immediate_points.size()):
+			_mesh.surface_set_color(_immediate_colors[pt_idx])
+			_mesh.surface_add_vertex(_immediate_points[pt_idx])
+			
+	for line: Dictionary in _lines:
 		_mesh.surface_set_color(line.color)
 		_mesh.surface_add_vertex(line.p1)
 		_mesh.surface_set_color(line.color)
 		_mesh.surface_add_vertex(line.p2)
+		
 	_mesh.surface_end()
+	
+	_immediate_points.clear()
+	_immediate_colors.clear()
+
+
+func _add_line(p1: Vector3, p2: Vector3, color: Color, lifetime: float) -> void:
+	if lifetime <= 0.0:
+		_immediate_points.append(p1)
+		_immediate_points.append(p2)
+		_immediate_colors.append(color)
+		_immediate_colors.append(color)
+	else:
+		_lines.append({
+			"p1": p1,
+			"p2": p2,
+			"color": color,
+			"time": lifetime
+		})
 
 
 func line(p1: Vector3, p2: Vector3, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
-	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
-		
-	_lines.append({
-		"p1": p1,
-		"p2": p2,
-		"color": color,
-		"time": lifetime
-	})
+	if not OS.is_debug_build(): return
+	_add_line(p1, p2, color, lifetime)
 
 
 func cube(position: Vector3, size: Vector3, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
+	if not OS.is_debug_build(): return
 	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
-	
-	var hs := size / 2.0
-	
+	var hs: Vector3 = size / 2.0
 	var p0: Vector3 = position + Vector3(-hs.x, -hs.y, -hs.z)
 	var p1: Vector3 = position + Vector3( hs.x, -hs.y, -hs.z)
 	var p2: Vector3 = position + Vector3( hs.x, -hs.y,  hs.z)
@@ -99,25 +114,16 @@ func cube(position: Vector3, size: Vector3, color: Color = Color.WHITE, lifetime
 		[p0, p4], [p1, p5], [p2, p6], [p3, p7]
 	]
 	
-	for edge in edges:
-		_lines.append({
-			"p1": edge[0],
-			"p2": edge[1],
-			"color": color,
-			"time": lifetime
-		})
+	for edge: Array in edges:
+		_add_line(edge[0], edge[1], color, lifetime)
 
 
 func sphere(position: Vector3, radius: float, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
+	if not OS.is_debug_build(): return
 	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
+	const SEGMENTS: int = 32
 	
-	const SEGMENTS = 64
-	
-	for i in range(SEGMENTS):
+	for i: int in range(SEGMENTS):
 		var a1: float = (i * TAU) / SEGMENTS
 		var a2: float = ((i + 1) * TAU) / SEGMENTS
 		
@@ -126,88 +132,49 @@ func sphere(position: Vector3, radius: float, color: Color = Color.WHITE, lifeti
 		var cos2: float = cos(a2) * radius
 		var sin2: float = sin(a2) * radius
 		
-		_lines.append({
-			"p1": position + Vector3(cos1, 0.0, sin1),
-			"p2": position + Vector3(cos2, 0.0, sin2),
-			"color": color, "time": lifetime
-		})
-		
-		_lines.append({
-			"p1": position + Vector3(cos1, sin1, 0.0),
-			"p2": position + Vector3(cos2, sin2, 0.0),
-			"color": color, "time": lifetime
-		})
-		
-		_lines.append({
-			"p1": position + Vector3(0.0, cos1, sin1),
-			"p2": position + Vector3(0.0, cos2, sin2),
-			"color": color, "time": lifetime
-		})
+		_add_line(position + Vector3(cos1, 0.0, sin1), position + Vector3(cos2, 0.0, sin2), color, lifetime)
+		_add_line(position + Vector3(cos1, sin1, 0.0), position + Vector3(cos2, sin2, 0.0), color, lifetime)
+		_add_line(position + Vector3(0.0, cos1, sin1), position + Vector3(0.0, cos2, sin2), color, lifetime)
 
 
 func cylinder(p_start: Vector3, p_end: Vector3, radius: float, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
+	if not OS.is_debug_build(): return
 	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
-	
-	const SEGMENTS: int = 64
-	
+	const SEGMENTS: int = 32
 	var axis: Vector3 = (p_end - p_start).normalized()
-	
-	if axis.is_zero_approx():
-		return
+	if axis.is_zero_approx(): return
 	
 	var ref: Vector3 = Vector3.UP if abs(axis.dot(Vector3.UP)) < 0.99 else Vector3.RIGHT
 	var tangent: Vector3 = axis.cross(ref).normalized() * radius
 	var bitangent: Vector3 = axis.cross(tangent).normalized() * radius
 
-	for i in range(SEGMENTS):
+	for i: int in range(SEGMENTS):
 		var a1: float = (i * TAU) / SEGMENTS
 		var a2: float = ((i + 1) * TAU) / SEGMENTS
 		
 		var vec1: Vector3 = (tangent * cos(a1)) + (bitangent * sin(a1))
 		var vec2: Vector3 = (tangent * cos(a2)) + (bitangent * sin(a2))
 		
-		_lines.append({
-			"p1": p_start + vec1, "p2": p_start + vec2,
-			"color": color, "time": lifetime
-		})
-		
-		_lines.append({
-			"p1": p_end + vec1, "p2": p_end + vec2,
-			"color": color, "time": lifetime
-		})
+		_add_line(p_start + vec1, p_start + vec2, color, lifetime)
+		_add_line(p_end + vec1, p_end + vec2, color, lifetime)
 
 	var angles: Array[float] = [0.0, PI / 2.0, PI, 3.0 * PI / 2.0]
-	for angle in angles:
+	for angle: float in angles:
 		var offset: Vector3 = (tangent * cos(angle)) + (bitangent * sin(angle))
-		_lines.append({
-			"p1": p_start + offset,
-			"p2": p_end + offset,
-			"color": color,
-			"time": lifetime
-		})
+		_add_line(p_start + offset, p_end + offset, color, lifetime)
 
 
 func capsule(p_start: Vector3, p_end: Vector3, radius: float, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
+	if not OS.is_debug_build(): return
 		
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
-	
-	const SEGMENTS: int = 64
-	const HALF_SEGMENTS: int = 32
+	const SEGMENTS: int = 32
+	const HALF_SEGMENTS: int = 16
 	
 	var to_end: Vector3 = p_end - p_start
 	var distance: float = to_end.length()
 	var axis: Vector3 = to_end.normalized()
 	
-	if axis.is_zero_approx():
-		return
-		
+	if axis.is_zero_approx(): return
 	if distance < radius * 2.0:
 		sphere((p_start + p_end) * 0.5, radius, color, lifetime)
 		return
@@ -218,100 +185,65 @@ func capsule(p_start: Vector3, p_end: Vector3, radius: float, color: Color = Col
 	
 	var t_rad: Vector3 = tangent * radius
 	var b_rad: Vector3 = bitangent * radius
-	
 	var cap_start: Vector3 = p_start + (axis * radius)
 	var cap_end: Vector3 = p_end - (axis * radius)
 
-	for i in range(SEGMENTS):
+	for i: int in range(SEGMENTS):
 		var a1: float = (i * TAU) / SEGMENTS
 		var a2: float = ((i + 1) * TAU) / SEGMENTS
-		
 		var vec1: Vector3 = (t_rad * cos(a1)) + (b_rad * sin(a1))
 		var vec2: Vector3 = (t_rad * cos(a2)) + (b_rad * sin(a2))
-		
-		_lines.append({"p1": cap_start + vec1, "p2": cap_start + vec2, "color": color, "time": lifetime})
-		_lines.append({"p1": cap_end + vec1, "p2": cap_end + vec2, "color": color, "time": lifetime})
+		_add_line(cap_start + vec1, cap_start + vec2, color, lifetime)
+		_add_line(cap_end + vec1, cap_end + vec2, color, lifetime)
 
 	var angles: Array[float] = [0.0, PI / 2.0, PI, 3.0 * PI / 2.0]
-	for angle in angles:
-		var offset := (t_rad * cos(angle)) + (b_rad * sin(angle))
-		_lines.append({"p1": cap_start + offset, "p2": cap_end + offset, "color": color, "time": lifetime})
+	for angle: float in angles:
+		var offset: Vector3 = (t_rad * cos(angle)) + (b_rad * sin(angle))
+		_add_line(cap_start + offset, cap_end + offset, color, lifetime)
 
-	for i in range(HALF_SEGMENTS):
+	for i: int in range(HALF_SEGMENTS):
 		var a1: float = (i * PI) / HALF_SEGMENTS
 		var a2: float = ((i + 1) * PI) / HALF_SEGMENTS
 		
 		var arc_t1_start: Vector3 = (t_rad * cos(a1)) - (axis * radius * sin(a1))
 		var arc_t2_start: Vector3 = (t_rad * cos(a2)) - (axis * radius * sin(a2))
-		_lines.append({"p1": cap_start + arc_t1_start, "p2": cap_start + arc_t2_start, "color": color, "time": lifetime})
+		_add_line(cap_start + arc_t1_start, cap_start + arc_t2_start, color, lifetime)
 		
 		var arc_b1_start: Vector3 = (b_rad * cos(a1)) - (axis * radius * sin(a1))
 		var arc_b2_start: Vector3 = (b_rad * cos(a2)) - (axis * radius * sin(a2))
-		_lines.append({"p1": cap_start + arc_b1_start, "p2": cap_start + arc_b2_start, "color": color, "time": lifetime})
+		_add_line(cap_start + arc_b1_start, cap_start + arc_b2_start, color, lifetime)
 
 		var arc_t1_end: Vector3 = (t_rad * cos(a1)) + (axis * radius * sin(a1))
 		var arc_t2_end: Vector3 = (t_rad * cos(a2)) + (axis * radius * sin(a2))
-		_lines.append({"p1": cap_end + arc_t1_end, "p2": cap_end + arc_t2_end, "color": color, "time": lifetime})
+		_add_line(cap_end + arc_t1_end, cap_end + arc_t2_end, color, lifetime)
 		
 		var arc_b1_end: Vector3 = (b_rad * cos(a1)) + (axis * radius * sin(a1))
 		var arc_b2_end: Vector3 = (b_rad * cos(a2)) + (axis * radius * sin(a2))
-		_lines.append({"p1": cap_end + arc_b1_end, "p2": cap_end + arc_b2_end, "color": color, "time": lifetime})
+		_add_line(cap_end + arc_b1_end, cap_end + arc_b2_end, color, lifetime)
 
 
 func cross(position: Vector3, size: float, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
-	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
-	
+	if not OS.is_debug_build(): return
 	var h: float = size / 2.0
-	
-	_lines.append({
-		"p1": position + Vector3(-h, 0.0, 0.0),
-		"p2": position + Vector3(h, 0.0, 0.0),
-		"color": color, "time": lifetime
-	})
-	
-	_lines.append({
-		"p1": position + Vector3(0.0, -h, 0.0),
-		"p2": position + Vector3(0.0, h, 0.0),
-		"color": color, "time": lifetime
-	})
-	
-	_lines.append({
-		"p1": position + Vector3(0.0, 0.0, -h),
-		"p2": position + Vector3(0.0, 0.0, h),
-		"color": color, "time": lifetime
-	})
+	_add_line(position + Vector3(-h, 0.0, 0.0), position + Vector3(h, 0.0, 0.0), color, lifetime)
+	_add_line(position + Vector3(0.0, -h, 0.0), position + Vector3(0.0, h, 0.0), color, lifetime)
+	_add_line(position + Vector3(0.0, 0.0, -h), position + Vector3(0.0, 0.0, h), color, lifetime)
+
 
 func circle(position: Vector3, direction: Vector3 = Vector3.FORWARD, radius: float = 1.0, color: Color = Color.WHITE, lifetime: float = 0.0) -> void:
-	if not OS.is_debug_build():
-		return
+	if not OS.is_debug_build(): return
 	
-	if lifetime <= 0.0:
-		lifetime = get_process_delta_time() + EPSILON
+	const SEGMENTS: int = 32
+	var normal: Vector3 = direction.normalized()
+	if normal.is_zero_approx(): return
 	
-	const SEGMENTS: int = 64
-	
-	var normal := direction.normalized()
-	if normal.is_zero_approx():
-		return
-	
-	var ref := Vector3.UP if abs(normal.dot(Vector3.UP)) < 0.99 else Vector3.RIGHT
-	var tangent := normal.cross(ref).normalized() * radius
-	var bitangent := normal.cross(tangent).normalized() * radius
+	var ref: Vector3 = Vector3.UP if abs(normal.dot(Vector3.UP)) < 0.99 else Vector3.RIGHT
+	var tangent: Vector3 = normal.cross(ref).normalized() * radius
+	var bitangent: Vector3 = normal.cross(tangent).normalized() * radius
 
-	for i in range(SEGMENTS):
-		var a1 := (i * TAU) / SEGMENTS
-		var a2 := ((i + 1) * TAU) / SEGMENTS
-		
-		var p1 := position + (tangent * cos(a1)) + (bitangent * sin(a1))
-		var p2 := position + (tangent * cos(a2)) + (bitangent * sin(a2))
-		
-		_lines.append({
-			"p1": p1,
-			"p2": p2,
-			"color": color,
-			"time": lifetime
-		})
+	for i: int in range(SEGMENTS):
+		var a1: float = (i * TAU) / SEGMENTS
+		var a2: float = ((i + 1) * TAU) / SEGMENTS
+		var p1: Vector3 = position + (tangent * cos(a1)) + (bitangent * sin(a1))
+		var p2: Vector3 = position + (tangent * cos(a2)) + (bitangent * sin(a2))
+		_add_line(p1, p2, color, lifetime)
--- scene/test.gd
+++ scene/test.gd
@@ -1,24 +1,8 @@
 @tool
 extends Node
 
-#func _ready() -> void:
-	#TinyDrawDebug.cross(Vector3.UP * 10.0, 0.25, Color.RED, 30.0)
-	#TinyDrawDebug.cross(Vector3.FORWARD * 3.0, 0.25, Color.RED, 30.0)
-	#TinyDrawDebug.capsule(Vector3.UP * 10.0, Vector3.FORWARD * 3.0, 1.0, Color.CORAL, 30.0)
-	#
-	#TinyDrawDebug.cube(Vector3.UP * 5.0, Vector3(1.0, 2.0, 3.0), Color.PURPLE, 5.0)
-
-
 func _process(delta: float) -> void:
-	#TinyDrawDebug.line(Vector3(1.0, 0.0, 1.0), Vector3(1.0, 2.0, 1.0), Color.RED)
-	#TinyDrawDebug.cube(Vector3(2.0, 0.5, 1.0), Vector3(1.0, 1.0, 1.0), Color.GREEN)
-	#TinyDrawDebug.circle(Vector3(3.5, 0.5, 1.0), Vector3.FORWARD, 0.5, Color.BLUE)
-	#TinyDrawDebug.sphere(Vector3(5.0, 0.5, 1.0), 0.5, Color.AQUA)
-	#TinyDrawDebug.cross(Vector3(6.5, 0.5, 1.0), 1.0, Color.CRIMSON)
-	#TinyDrawDebug.cylinder(Vector3(8.0, 0.0, 1.0), Vector3(8.0, 1.0, 1.0), 0.25, Color.BISQUE)
-	#TinyDrawDebug.capsule(Vector3(9.5, 0.0, 1.0), Vector3(9.5, 1.25, 1.0), 0.25, Color.DARK_ORANGE)
-	
-	for i in 3:
+	for i in 10:
 		TinyDrawDebug.line(Vector3(1.0, 0.0, 1.1 * i), Vector3(1.0, 2.0, 1.1 * i), Color.RED)
 		TinyDrawDebug.cube(Vector3(2.0, 0.5, 1.1 * i), Vector3(1.0, 1.0, 1.0), Color.GREEN)
 		TinyDrawDebug.circle(Vector3(3.5, 0.5, 1.1 * i), Vector3.FORWARD, 0.5, Color.BLUE)
